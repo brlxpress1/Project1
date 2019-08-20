ad9e0ba88ec2038055990e4be1b8dbe013ad18a2
turzoxpress <turzoxpress@gmail.com>
2019-08-20 11:17:14 +0600

    Company Signup with Dashboard 2

--- app/src/main/java/com/tur/job1/company/Company_Login_1.java
+++ app/src/main/java/com/tur/job1/company/Company_Login_1.java
@@ -1,17 +1,74 @@
 package com.tur.job1.company;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.hbb20.CountryCodePicker;
+import com.tur.job1.Intro;
 import com.tur.job1.R;
+import com.tur.job1.job_seeker.Job_Seeker_Login;
+import com.tur.job1.job_seeker.Job_Seeker_Login_Verify;
+import com.tur.job1.job_seeker.Job_Seeker_Verify_1;
+import com.tur.job1.others.Connectivity;
+import com.tur.job1.others.ConstantsHolder;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
+import es.dmoral.toasty.Toasty;
 
 public class Company_Login_1 extends AppCompatActivity {
 
+    private String TAG = "Company_Login_1";
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.6F);
 
-    private String TAG = "Company_Login_1";
+
+
+    private Button next;
+    //private EditText name;
+    private TextView countryCode;
+    private EditText phone_number;
+    private LinearLayout countryCodePanel;
+
+
+    private CountryCodePicker ccp;
+    private Dialog dialog;
+
+
+
+    //public String otpID;
+    private String userName;
+    private String userPhone;
+    private String userPurePhone;
+
+
+    private TextView signup_helper_txt;
+
+
+
 
 
 
@@ -23,5 +80,323 @@ public class Company_Login_1 extends AppCompatActivity {
         setContentView(R.layout.activity_company_login_1);
 
 
+        next = (Button)findViewById(R.id.next_button);
+        //name = (EditText)findViewById(R.id.name_input);
+        countryCode = (TextView)findViewById(R.id.country_code_input);
+        phone_number = (EditText)findViewById(R.id.phone_number_input);
+
+        ccp = (CountryCodePicker) findViewById(R.id.ccp);
+        countryCodePanel = (LinearLayout)findViewById(R.id.country_code_panel);
+
+        signup_helper_txt = (TextView)findViewById(R.id.signup_helper_txt);
+
+        //-- initial calls
+/*
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("M/d/yy hh:mm a");
+        gson = gsonBuilder.create();
+        */
+
+
+
+        //------------------
+
+
+
+
+
+
+        phone_number.setOnFocusChangeListener((view, b) -> {
+            if (view.isFocused()) {
+                // Do whatever you want when the EditText is focused
+                // Example:
+                //editTextFrom.setText("Focused!");
+                phone_number.setBackgroundResource(R.drawable.edittext_box_border);
+            }
+            else{
+                phone_number.setBackgroundResource(R.drawable.shape_with_border1);
+            }
+        });
+
+
+
+        //-----------------
+
+
+
+        // showLoadingBarAlert();
+
+
+        if(userPurePhone != null && !userPurePhone.equalsIgnoreCase("")){
+
+            phone_number.setText(userPurePhone);
+
+        }
+
+        countryCodePanel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                countryCodePanel.startAnimation(buttonClick);
+
+                ccp.launchCountrySelectionDialog();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                next.startAnimation(buttonClick);
+
+                //-- Check if user is connected with the internet
+                if (Connectivity.isConnected(Company_Login_1.this)) {
+
+
+                    //-- Check if name input field is empty
+
+
+                    //-- Check if phone number input field is empty
+                    String temp2 = phone_number.getText().toString().trim();
+                    if(temp2.equalsIgnoreCase("") || temp2 == null){
+
+                        Toasty.error(Company_Login_1.this, "Please enter your phone number!", Toast.LENGTH_LONG, true).show();
+                    }else {
+
+
+
+                        //String tempPhone = ccp.getSelectedCountryCodeWithPlus()+phone_number.getText().toString().trim();
+                        String tempPhone = countryCode.getText().toString().trim()+phone_number.getText().toString().trim();
+
+                        //Toasty.success(Job_Seeker_Verify_1.this, "Name : "+tempName+"\nPhone : "+tempPhone, Toast.LENGTH_LONG, true).show();
+
+                        //showLoadingBarAlert();
+                        //-- task
+
+                        userPhone = tempPhone;
+                        userPurePhone = removePlusFromPhone(userPhone);
+
+
+
+                        /*
+
+
+                         */
+
+                        // Intent openSecondVerifier = new Intent(Job_Seeker_Verify_1.this,Job_Seeker_Verify_2.class);
+                        // startActivity(openSecondVerifier);
+                        // finish();
+
+                        phone_number_check(removePlusFromPhone(userPhone));
+                    }
+
+
+
+
+                } else {
+
+
+                    Toasty.error(Company_Login_1.this, "You have no internet access! Please turn on your WiFi or mobile data.", Toast.LENGTH_LONG, true).show();
+
+                }
+
+
+            }
+        });
+
+        //--
+
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+
+            @Override
+
+            public void onCountrySelected() {
+
+                ccp.startAnimation(buttonClick);
+                countryCode.setText(ccp.getSelectedCountryCodeWithPlus().toString().trim());
+
+
+
+            }
+
+        });
+
+        signup_helper_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent openCVwindow = new Intent(Company_Login_1.this, Company_Signup_1.class);
+                startActivity(openCVwindow);
+                finish();
+            }
+        });
+
+        //-----------------
+
+
     }
+
+    private void showLoadingBarAlert(){
+
+
+        dialog = new Dialog(Company_Login_1.this);
+
+        dialog.setContentView(R.layout.loading);
+
+        dialog.setTitle("Please wait!");
+
+        dialog.setCancelable(false);
+
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        dialog.show();
+
+    }
+
+
+
+    private void hideLoadingBar(){
+
+
+
+        dialog.dismiss();
+
+    }
+
+    // this method will store the info of user to  database
+    private void phone_number_check(String userPhone) {
+
+
+        JSONObject parameters = new JSONObject();
+        try {
+            parameters.put("userPhoneNumber", userPhone);
+            parameters.put("userType",1);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG,parameters.toString());
+
+        RequestQueue rq = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, ConstantsHolder.rawServer+ConstantsHolder.phoneCheck, parameters, new com.android.volley.Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        String respo=response.toString();
+                        Log.d(TAG,respo);
+
+
+                        parsePhoneCheckData(response);
+
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toasty.error(Company_Login_1.this, "Server error,please check your internet connection!", Toast.LENGTH_LONG, true).show();
+                        //Toast.makeText(Login_A.this, "Something wrong with Api", Toast.LENGTH_SHORT).show();
+                        // hideLoadingBar();
+
+                    }
+                }){
+
+            /** Passing some request headers* */
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                //headers.put("apiKey", "xxxxxxxxxxxxxxx");
+                return headers;
+            }
+        };
+        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(30000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rq.getCache().clear();
+        rq.add(jsonObjectRequest);
+
+    }
+
+    // if the signup successfull then this method will call and it store the user info in local
+    public void parsePhoneCheckData(JSONObject jobj){
+        //JSONObject jsonObject= jobj;
+        boolean userExist = jobj.optBoolean("userExist");
+        if(userExist){
+
+            JSONObject jobSeekerModel = jobj.optJSONObject("companyModel");
+            String userId = jobSeekerModel.optString("userId");
+
+            SharedPreferences.Editor editor = getSharedPreferences("CompanyData", MODE_PRIVATE).edit();
+            editor.putString("userid", userId);
+            editor.putString("userphone", userPhone);
+            editor.apply();
+
+
+            SharedPreferences.Editor typeEditor = getSharedPreferences("UserType", MODE_PRIVATE).edit();
+            typeEditor.putInt("type", 1);
+            typeEditor.apply();
+
+
+
+            Intent openSecondVerifier = new Intent(this, Company_Login_2.class);
+            startActivity(openSecondVerifier);
+            finish();
+
+
+
+
+
+        }else {
+
+            Toasty.error(Company_Login_1.this,"We can't recognize this number! Try Sign Up now.",Toast.LENGTH_LONG, true).show();
+
+
+        }
+
+    }
+
+
+    private String removePlusFromPhone(String ph){
+
+        String temp = "";
+
+        for(int i=0; i<ph.length(); i++){
+
+            if(i==0){
+
+            }else{
+                temp = temp + ph.charAt(i);
+            }
+        }
+
+        return  temp;
+    }
+
+
+
+    @Override
+    public void onBackPressed() {
+
+
+
+        Intent introOpener = new Intent(Company_Login_1.this, Intro.class);
+        startActivity(introOpener);
+        finish();
+
+    }
+
+
+
 }
+
--- app/src/main/java/com/tur/job1/company/Company_Login_2.java
+++ app/src/main/java/com/tur/job1/company/Company_Login_2.java
@@ -1,19 +1,58 @@
 package com.tur.job1.company;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
 import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.tur.job1.R;
+import com.tur.job1.job_seeker.Job_Seeker_Dashboard;
+import com.tur.job1.job_seeker.Job_Seeker_Login;
+import com.tur.job1.job_seeker.Job_Seeker_Login_Verify;
+import com.tur.job1.others.Connectivity;
 
+import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import es.dmoral.toasty.Toasty;
 
 public class Company_Login_2 extends AppCompatActivity {
 
     private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.6F);
-
     private String TAG = "Company_Login_2";
 
 
+    private Dialog dialog;
+    private EditText otpCodeInput;
+    private Button verifyButton;
+    private TextView resendButton;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks
+
+            verificationCallbacks;
+    private PhoneAuthCredential credential;
+
+    private String otpID;
+    private FirebaseAuth fbAuth;
 
 
 
@@ -23,5 +62,464 @@ public class Company_Login_2 extends AppCompatActivity {
         setContentView(R.layout.activity_company_login_2);
 
 
+        otpCodeInput = (EditText)findViewById(R.id.otp_code_input);
+        verifyButton = (Button)findViewById(R.id.verify_button);
+        resendButton = (TextView)findViewById(R.id.resend_button);
+
+
+        //-- Changing selection effect of input fields
+        otpCodeInput.setOnFocusChangeListener((view, b) -> {
+            if (view.isFocused()) {
+                // Do whatever you want when the EditText is focused
+                // Example:
+                //editTextFrom.setText("Focused!");
+                otpCodeInput.setBackgroundResource(R.drawable.edittext_box_border);
+            }
+            else{
+                otpCodeInput.setBackgroundResource(R.drawable.shape_with_border1);
+            }
+        });
+
+
+
+        //-----------------
+
+
+
+
+        fbAuth = FirebaseAuth.getInstance();
+
+        showLoadingBarAlert();
+
+        SharedPreferences prefs = getSharedPreferences("CompanyData", MODE_PRIVATE);
+
+        String userPhone = prefs.getString("userphone", "");
+
+
+        send_otp_by_firebase(userPhone);
+
+        resendButton.setText("Did not get the OTP code? "+ Html.fromHtml("<p><u>Resend</u></p>"));
+
+        verifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                verifyButton.startAnimation(buttonClick);
+
+                if (Connectivity.isConnected(Company_Login_2.this)) {
+
+
+                    verifyCode();
+
+                } else {
+
+                    Toasty.error(Company_Login_2.this, "You have no internet access! Please turn on your WiFi or mobile data.", Toast.LENGTH_LONG, true).show();
+
+                }
+
+            }
+        });
+
+        resendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                resendButton.startAnimation(buttonClick);
+
+                if (Connectivity.isConnected(Company_Login_2.this)) {
+
+
+                    resendCode();
+
+                } else {
+
+                    Toasty.error(Company_Login_2.this, "You have no internet access! Please turn on your WiFi or mobile data.", Toast.LENGTH_LONG, true).show();
+
+                }
+
+            }
+        });
+
+
+
+
     }
+
+    private void showLoadingBarAlert(){
+
+
+        dialog = new Dialog(Company_Login_2.this);
+
+        dialog.setContentView(R.layout.loading);
+
+        dialog.setTitle("Please wait!");
+
+        dialog.setCancelable(false);
+
+
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        dialog.show();
+
+    }
+
+
+
+    private void hideLoadingBar(){
+
+
+
+        dialog.dismiss();
+
+    }
+
+    //-- OTP related task
+
+    //--
+
+    private void send_otp_by_firebase(String number){
+
+
+        SharedPreferences prefs = getSharedPreferences("CompanyData", MODE_PRIVATE);
+        //Log.d(TAG,prefs.getString("userid", "null"));
+
+        String userName = prefs.getString("username", "");
+        String userPhone = prefs.getString("userphone", "");
+        Log.d(TAG,"From shared preference : "+userName+"--------------"+userPhone);
+
+        if(userName.equalsIgnoreCase("")){
+
+            // Go to sign up page
+        }else {
+
+            if(userPhone.equalsIgnoreCase("")){
+
+                // Go to sign up page
+            }else {
+
+
+                //registerUser(userName,userPhone);
+
+
+
+                verificationCallBack();
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+
+                        number,        // Phone number to verify
+
+                        60,                 // Timeout duration
+
+                        TimeUnit.SECONDS,   // Unit of timeout
+
+                        this,               // Activity (for callback binding)
+
+                        verificationCallbacks);
+
+
+
+
+
+                //--
+
+
+            }
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+    private void verificationCallBack(){
+
+
+
+        verificationCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+
+
+            @Override
+
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+
+                // This callback will be invoked in two situations:
+
+                // 1 - Instant verification. In some cases the phone number can be instantly
+
+                //     verified without needing to send or enter a verification code.
+
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+
+                //     detect the incoming verification SMS and perform verification without
+
+                //     user action.
+
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+
+
+
+                //credential = PhoneAuthProvider.getCredential(verificationId, code);
+
+                signInWithPhoneAuthCredential(credential);
+
+            }
+
+
+
+            @Override
+
+            public void onVerificationFailed(FirebaseException e) {
+
+                // This callback is invoked in an invalid request for verification is made,
+
+                // for instance if the the phone number format is not valid.
+
+                Log.w(TAG, "onVerificationFailed", e);
+
+
+
+                //Toast.makeText(Verification.this, "Enter Correct Number.", Toast.LENGTH_SHORT).show();
+
+                Toasty.error(Company_Login_2.this, "Enter a valid phone number!", Toast.LENGTH_LONG, true).show();
+
+
+                SharedPreferences.Editor editor = getSharedPreferences("CompanyData", MODE_PRIVATE).edit();
+                editor.putString("userid", "");
+                //editor.putString("userphone", userPhone);
+                editor.apply();
+
+                Intent openSecondVerifier = new Intent(Company_Login_2.this, Job_Seeker_Login.class);
+                startActivity(openSecondVerifier);
+                finish();
+
+
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+
+                    // Invalid request
+
+                    // ...
+
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+
+                    // The SMS quota for the project has been exceeded
+
+                    // ...
+
+                }
+
+
+
+                // Show a message and update the UI
+
+                // ...
+
+            }
+
+
+
+            @Override
+            public void onCodeSent(String verificationId,
+
+                                   PhoneAuthProvider.ForceResendingToken token) {
+
+                // The SMS verification code has been sent to the provided phone number, we
+
+                // now need to ask the user to enter the code and then construct a credential
+
+                // by combining the code with a verification ID.
+
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+
+
+                // Save verification ID and resending token so we can use them later
+
+
+
+                Log.d(TAG,verificationId+"------------"+token);
+
+                otpID = verificationId;
+
+                Toasty.info(Company_Login_2.this, "OTP code sent to your phone number, please enter the code.", Toast.LENGTH_LONG, true).show();
+                hideLoadingBar();
+
+
+
+
+
+
+                // ...
+
+            }
+
+        };
+
+    }
+
+
+
+
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+
+        fbAuth.signInWithCredential(credential)
+
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+
+                    @Override
+
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            // Sign in success, update UI with the signed-in user's information
+
+                            Log.d(TAG, "signInWithCredential:success");
+
+
+
+                            //--
+                            Intent openSecondVerifier = new Intent(Company_Login_2.this,Company_Dashboard.class);
+                            startActivity(openSecondVerifier);
+                            finish();
+
+                            //---------------------
+
+                            // registerUser(userNameLocal,userPhoneLocal);
+
+
+
+
+
+                        } else {
+
+                            // Sign in failed, display a message and update the UI
+
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+
+                                // The verification code entered was invalid
+
+                                //Toast.makeText(Verification.this, "Verification code is invalid!", Toast.LENGTH_LONG).show();
+
+                                Toasty.error(Company_Login_2.this, "OTP code is invalid! Please enter correct OTP code.", Toast.LENGTH_LONG, true).show();
+                                hideLoadingBar();
+
+
+
+
+
+                            }
+
+                        }
+
+                    }
+
+                });
+
+    }
+
+
+
+    private void verifyCode(){
+
+
+        //showLoadingBarAlert();
+        String temp = otpCodeInput.getText().toString().trim();
+
+        if(temp.equals("")){
+
+
+
+            //Toast.makeText(Verification.this, "You must enter the verification code!", Toast.LENGTH_SHORT).show();
+
+            Toasty.error(Company_Login_2.this, "Enter the verification code first!", Toast.LENGTH_LONG, true).show();
+
+
+
+        }else {
+
+
+
+            showLoadingBarAlert();
+
+
+
+            credential = PhoneAuthProvider.getCredential(otpID, temp);
+
+            signInWithPhoneAuthCredential(credential);
+
+        }
+
+
+
+    }
+
+
+
+    public void resendCode() {
+
+
+
+        SharedPreferences prefs = getSharedPreferences("CompanyData", MODE_PRIVATE);
+        //Log.d(TAG,prefs.getString("userid", ""));
+
+
+        String userPhone = prefs.getString("userphone", "");
+
+        String temp = userPhone;
+
+
+
+        if(temp.equalsIgnoreCase("")){
+
+        }else {
+
+
+            //Toasty.success(Job_Seeker_Login_Verify.this, "OTP code was sent to your phone number, please check.", Toast.LENGTH_LONG, true).show();
+
+            send_otp_by_firebase(temp);
+
+
+
+        }
+
+    }
+
+
+
+    //-------------------------
+
+
+
+
+
+
+    //---------------
+
+    @Override
+    public void onBackPressed() {
+
+
+
+        Intent openSecondVerifier = new Intent(Company_Login_2.this,Company_Login_1.class);
+        startActivity(openSecondVerifier);
+        finish();
+
+    }
+
+
 }
+
